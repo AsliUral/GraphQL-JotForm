67df9ba0c098c9f4cf6db1bea02c014ee08bb489
AsliUral <aasliuural@gmail.com>
2020-05-20 14:34:04 +0300

    #5 User Schema created and also created graphql server

---
+++ schema.js
@@ -0,0 +1,72 @@
+const axios = require("axios");
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLInt,
+    GraphQLList,
+} = require("graphql");
+
+//User Type
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields: () => ({
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        time_zone: { type: GraphQLString },
+        account_type: { type: GraphQLString },
+        status: { type: GraphQLString },
+        loginToGetSubmissions: { type: GraphQLInt },
+        pdf_designer_group: { type: GraphQLInt },
+    }),
+});
+
+// User Form Type
+const UserFormType = new GraphQLObjectType({
+    name: "UserFormType",
+    fields: () => ({
+        id: { type: GraphQLInt },
+        username: { type: GraphQLString },
+        type: { type: GraphQLString },
+        last_submission: { type: GraphQLString },
+        status: { type: GraphQLString },
+        height: { type: GraphQLInt },
+        count: { type: GraphQLInt },
+    }),
+});
+
+//Root Query
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                apiKey: { type: GraphQLString },
+            },
+            resolve(parentValue, args) {
+                return axios
+                    .get(`https://api.jotform.com/user?apiKey=${args.apiKey}`)
+                    .then((res) => res.data.content);
+            },
+        },
+        userForm: {
+            type: new GraphQLList(UserFormType),
+            args: {
+                apiKey: { type: GraphQLString },
+            },
+            resolve(parentValue, args) {
+                return axios
+                    .get(
+                        `https://api.jotform.com/user/forms?apiKey=${args.apiKey}`
+                    )
+                    .then((res) => res.data.content);
+            },
+        },
+    },
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+});
