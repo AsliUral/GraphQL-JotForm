a96c0c23e76de7ed405582b0424ad70560970186
AsliUral <aasliuural@gmail.com>
2020-06-25 12:42:33 +0300

    #9 Fixed bug: Sidebar css zIndex is changed

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -5,9 +5,16 @@ import SideBarButton from "../containers/SideBarButtonContainer";
 import SideBarContent from "../containers/SideBarContentContainer";
 
 function SideBar(props) {
+    let sideBarCss;
+    if (props.sidebarOpen) {
+        sideBarCss = {};
+    } else {
+        sideBarCss = { float: "right", position: "relative" };
+    }
     return (
-        <>
+        <div id={"debug1"} style={sideBarCss}>
             <Sidebar
+                id={"debug2"}
                 sidebar={<SideBarContent />}
                 open={props.sidebarOpen}
                 onSetOpen={props.onSetSidebarOpen}
@@ -19,11 +26,20 @@ function SideBar(props) {
                     sidebar: {
                         background: "white",
                         width: 300,
+                        zIndex: 2,
+                        insert: "unset",
+                        position: "absolute",
+                        top: 0,
+                        bottom: 0,
+                        transition: "transform .3s ease-out",
+                        WebkitTransition: "-webkit-transform .3s ease-out",
+                        willChange: "transform",
+                        overflowY: "auto",
                     },
                 }}
             />
             <SideBarButton />
-        </>
+        </div>
     );
 }
 
